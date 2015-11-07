a5c23c38645ddf37f70799713627a60095ad63d7
Manat Lertpeetipat <mlertpee@gmail.com>
2015-11-07 22:24:40 +0700

    Load data from an actual API server.

--- app/models/employee.js
+++ app/models/employee.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
-  phone: DS.attr('string')
-  // subordinates: DS.hasMany('subordinate')
+  phone: DS.attr('string'),
+  supervisor: DS.attr('string')
 });
